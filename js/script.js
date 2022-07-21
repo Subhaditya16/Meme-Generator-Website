5c79fe89da1866f30efa8c88861bd56166eee0f6
Subhaditya Bhattacharjee <subhaditya16@gmail.com>
2022-07-22 00:21:24 +0530

    Made folders for css and js

--- js/script.js
+++ js/script.js
@@ -105,4 +105,10 @@ function getTemplates() {
       container.innerHTML = html;
     });
 }
+// for (let i = array.length - 1; i > 0; i--) {
+//   const j = Math.floor(Math.random() * i);
+//   const temp = array[i];
+//   array[i] = array[j];
+//   array[j] = temp;
+// }
 getTemplates();
